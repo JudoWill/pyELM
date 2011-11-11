437797d06855772f209a7bad4da4449fd33dd739
Will Dampier <dampier.william@gmail.com>
2011-11-11 13:52:48 -0500

    wow, stupid mistake!

--- pyWebELM.py
+++ pyWebELM.py
@@ -83,10 +83,10 @@ def process_fasta_file(fasta_file, out_file, num_processes):
                 logging.warning('%s had %i matches' % (name, len(out)))
                 for elm, pos in out:
                     try:
-                        out = [name, elm] + extract_numbers(pos[0]) + [pos[1]]
+                        outrow = [name, elm] + extract_numbers(pos[0]) + [pos[1]]
                     except:
                         continue
-                    writer.writerow(out)
+                    writer.writerow(outrow)
             else:
                 logging.warning('%s had no ELMs' % name)
 
